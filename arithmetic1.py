8007ac6ff8f83bef497066fe33afad6d6f52f08a
Ravindra11697 <ravindraravindra8988@gmail.com>
2025-03-21 16:27:53 +0530

    Add files via upload

---
+++ arithmetic1.py
@@ -0,0 +1,7 @@
+a=10
+b=3
+print("sum:",a+b)
+print("sub:",a-b)
+print("mul:",a*b)
+print("div:",a/b)
+print("mod:",a%b) 
---
+++ circle.py
@@ -0,0 +1,4 @@
+d=int(input('enter diameter of a circle'))
+radius=d/2
+print('the radius of circle with diameter',d,'is',radius)
+
