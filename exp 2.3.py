9e700c5af69374582f65f576e0d34b8fb18ad697
Ravindra11697 <ravindraravindra8988@gmail.com>
2025-03-28 16:07:42 +0530

    EXPERIMENT-2

---
+++ exp 2.3.py
@@ -0,0 +1,23 @@
+num=int(input("enter a number:"))
+if num==0:
+    print("zero")
+if num==1:
+    print("one")
+if num==2:
+    print("two")
+if num==3:
+    print("three")
+if num==4:
+    print("four")
+if num==5:
+    print("five")
+if num==6:
+    print("six")
+if num==7:
+    print("seven")
+if num==8:
+    print("eight")
+if num==9:
+    print("nine")
+else:
+    print("number of range:")
