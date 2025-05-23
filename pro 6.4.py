bd45debb15474bedb5d0489d166b39b7baba3ea7
Ravindra11697 <ravindraravindra8988@gmail.com>
2025-05-23 16:16:25 +0530

    Add files via upload

---
+++ pro 6.4.py
@@ -0,0 +1,4 @@
+import turtle
+my_pen=turtle.Turtle()
+for i in range(4):
+    my_pen.forward(50)
---
+++ pro 6.6.py
@@ -0,0 +1,9 @@
+import turtle
+polygon=turtle.Turtle()
+my_num_sides=6
+my_side_length=70
+my_angle=360.0/my_num_sides
+for i in angle(my_num_sides):
+    polygon.forward(my_side_length)
+    polygon.forward(my_angle)
+    turtle.done()
